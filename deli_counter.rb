558fb6179ce0e5c1a9ec45f58ae1b27a271507aa
kwilki <kylewilkinson14@gmail.com>
2022-10-14 14:18:32 +1100

    commit tree 70c996296a15a35dff5d8ad7abf6d6de06ef69ea

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def line(deli)
+    if deli.empty?
+        puts "The line is currently empty."
+    else 
+        current_places = "The line is currently:"
+        deli.each.with_index(1) do |person, i|
+            current_places << " #{i}. #{person}"
+        end
+        puts current_places
+    end
+end
